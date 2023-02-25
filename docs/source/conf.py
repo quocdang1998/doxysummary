330962e3a4a62db340196bd42b52023f10719637
Nguyen Dinh Quoc Dang <quocdang1998@gmail.com>
2023-02-25 09:38:05 +0100

    Move breathe to extra_requires

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, quocdang1998'
 author = 'quocdang1998'
 
 # The full version, including alpha/beta/rc tags
-release = '2.2.1'
+release = '2.3.2'
 
 
 # -- General configuration ---------------------------------------------------
