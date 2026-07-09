e0b4ac9f92fd532a56883e52634567a7fe5a019d
quocdang1998 <quocdang1998@gmail.com>
2026-07-09 14:17:11 -0500

    Release 1.0.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, quocdang1998'
 author = 'quocdang1998'
 
 # The full version, including alpha/beta/rc tags
-release = '1.0.0'
+release = '1.0.1'
 
 
 # -- General configuration ---------------------------------------------------
